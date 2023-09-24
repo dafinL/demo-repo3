77ae41f2a94469ef829bca39f02b3620faf74788
DafinL <dafinlincan@yahoo.com>
2023-09-24 12:15:22 +0300

    add express server

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const http = require('http')
+const express = require('express')
 const { sum } = require('./functions.js')
 
-const server = http.createServer( (req, res)  => res.end("message for all the world !") ).listen(3000)
+//const server = http.createServer( (req, res)  => res.end("message for all the world") ).listen(3000)
+
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
 
 console.log(sum(2, 3))
